47bba06d40f6a0bf689f9267cf9bacb3f0ffc493
Elfesya ESEN <49937779+elfesyaesen@users.noreply.github.com>
2025-02-15 09:42:07 +0300

    kurucu method çakışması ortadan kaldırıldı.

--- system/engine/Controller.php
+++ system/engine/Controller.php
@@ -2,6 +2,9 @@
 
 namespace System\Engine;
 
+use System\Engine\Request;
+use System\Engine\Response;
+
 class Controller
 {
     use Response, Request;
@@ -13,6 +16,14 @@ class Controller
         } else {
             $this->data["csrf"] = $_SESSION["csrf"] = bin2hex(random_bytes(16));
         }
+
+        $this->server = $this->sanitize($_SERVER);
+        $this->headers = $this->getHeaders();
+        $this->get = $this->sanitize($_GET);
+        $this->post = $this->sanitize($this->parseInput('POST'));
+        $this->put = $this->sanitize($this->parseInput('PUT'));
+        $this->patch = $this->sanitize($this->parseInput('PATCH'));
+        $this->files = $this->sanitize($_FILES);
     }
 
     public function view(string $path, array $data = []): void
